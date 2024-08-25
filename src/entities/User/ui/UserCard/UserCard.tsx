3284b98fcd5c623cfcad168a496180fed3b06a6c
IrinaPerezhegina <perezeginaiv@mail.ru>
2024-08-25 20:53:24 +0500

    change netlify

--- src/entities/User/ui/UserCard/UserCard.tsx
+++ src/entities/User/ui/UserCard/UserCard.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 import { classNames } from 'shared/lib/classnames/classNames';
 import { memo, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
@@ -6,14 +7,13 @@ import { getFetchUsersData, getFetchUsersIsLoading } from 'entities/User/model/s
 import { fetchUsersList } from 'entities/User/model/services/fetchUsersData';
 import { useAppDispatch } from 'shared/lib/hooks/useAppDispatch/useAppDispatch';
 import { HStack, VStack } from 'shared/ui/Stack';
-import { Text } from 'shared/ui/Text/Text';
+import { Text, TextSize } from 'shared/ui/Text/Text';
 import { Button, ButtonTheme } from 'shared/ui/Button/Button';
-import { Icon } from 'shared/ui/Icon/Icon';
-import { FaHeart, FaRegHeart } from 'react-icons/fa';
 import { Avatar } from 'shared/ui/Avatar/Avatar';
 import { AppLink } from 'shared/ui/AppLink/AppLink';
 import { RoutePath } from 'shared/config/routeConfig/routeConfig';
 import Profile from 'shared/assets/profile.jpg';
+import { Skeleton } from 'shared/ui/Skeleton/Skeleton';
 import cls from './UserCard.module.scss';
 
 interface UserCardProps {
@@ -22,20 +22,21 @@ className?: string;
 
 export const UserCard = memo(({ className }:UserCardProps) => {
     const { id } = useParams<{id:string}>();
-    const user = useSelector(getFetchUsersData);
+    const user = useSelector(getFetchUsersData).find((el) => el.id === Number(id));
     const isLoading = useSelector(getFetchUsersIsLoading);
     const dispatch = useAppDispatch();
-    // console.log(id, user);
+    console.log(id, user, isLoading);
     useEffect(() => {
         dispatch(fetchUsersList());
-    }, [dispatch]);
+    }, [dispatch, id]);
     console.log(user);
-    const item = user[0];
-    if (isLoading) {
-        return null;
+    const item = user;
+    if (!user) {
+        return <Skeleton />;
     } return (
         <VStack
-            gap="32"
+            gap="50"
+            justify="between"
             max
             className={classNames(cls.UserCard, {}, [className])}
         >
@@ -48,70 +49,123 @@ export const UserCard = memo(({ className }:UserCardProps) => {
                     </Button>
                 </AppLink>
             </HStack>
-            <VStack max gap="8" justify="between">
+            <VStack gap="50" className={cls.content}>
                 <HStack gap="16">
                     <Avatar src={Profile} />
                 </HStack>
-                <HStack gap="8" max justify="between">
+                <HStack gap="8" max>
                     <VStack align="start">
-                        <HStack align="start" gap="8">
+                        <HStack align="center" gap="16" justify="between">
                             <Text
+                                title="Username:"
+                            />
+                            <Text
+                                size={TextSize.L}
                                 title={item.username}
                             />
+                        </HStack>
+                        <HStack align="center" gap="16" justify="between">
                             <Text
-                                title="("
+                                title="Name:"
                             />
                             <Text
+                                size={TextSize.L}
                                 title={item.name}
                             />
+                        </HStack>
+                        <HStack align="center" gap="16" justify="center">
                             <Text
-                                title=")"
+                                title="Email:"
+                            />
+                            <Text
+                                size={TextSize.L}
+                                title={item.email}
                             />
                         </HStack>
-                        <HStack align="start" gap="8">
+                        <HStack align="center" gap="16" justify="center">
                             <Text
-                                text="email:"
+                                title="Phone:"
                             />
                             <Text
-                                text={item.email}
-                            />
-                        </HStack>
-                        <HStack align="start" gap="8">
-                            <Text
-                                text="phone:"
-                            />
-                            <Text
-                                text={item.phone}
+                                size={TextSize.L}
+                                title={item.phone}
                             />
                         </HStack>
-                        <HStack align="start" gap="8">
+                        <HStack align="center" gap="16" justify="center">
                             <Text
-                                text="city:"
+                                title="Website:"
                             />
                             <Text
-                                text={item.address.city}
+                                size={TextSize.L}
+                                title={item.website}
+                            />
+                        </HStack>
+                        <HStack align="center" gap="16" justify="center">
+                            <Text
+                                title="Company:"
+                            />
+                            <Text
+                                size={TextSize.L}
+                                title={item.company.name}
+                            />
+                        </HStack>
+                        <HStack align="center" gap="16" justify="center">
+                            <Text
+                                title="CatchPhrase:"
+                            />
+                            <Text
+                                size={TextSize.L}
+                                title={item.company.catchPhrase}
+                            />
+                        </HStack>
+                        <HStack align="center" gap="16" justify="center">
+                            <Text
+                                title="City:"
+                            />
+                            <Text
+                                size={TextSize.L}
+                                title={item.address.city}
+                            />
+                        </HStack>
+                        <HStack align="center" gap="16" justify="center">
+                            <Text
+                                title="Street:"
+                            />
+                            <Text
+                                size={TextSize.L}
+                                title={item.address.street}
+                            />
+                        </HStack>
+                        <HStack align="center" gap="16" justify="center">
+                            <Text
+                                title="Suite:"
+                            />
+                            <Text
+                                size={TextSize.L}
+                                title={item.address.suite}
+                            />
+                        </HStack>
+                        <HStack align="center" gap="16" justify="center">
+                            <Text
+                                title="Zipcode:"
+                            />
+                            <Text
+                                size={TextSize.L}
+                                title={item.address.zipcode}
                             />
                         </HStack>
                     </VStack>
 
                 </HStack>
-
             </VStack>
-            <HStack max justify="start" gap="32">
-                <AppLink
-                    to={`${RoutePath.card}/${item.id}`}
-                >
-                    <Button>Подробнее о профиле</Button>
-                </AppLink>
-                <Button theme={ButtonTheme.CLEAR}>
-                    <Icon
-                        Svg={item.like ? FaHeart : FaRegHeart}
-                        fontSize={40}
-                        className={cls.like}
-                    />
-                </Button>
-
-            </HStack>
+            <VStack>
+                <Text title="About me" />
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus aperiam dicta harum assumenda recusandae corrupti nemo quo, fugit explicabo. Veniam, autem fugiat suscipit iusto voluptatem delectus corrupti sint beatae consectetur.
+                Eius quos neque quidem temporibus et pariatur incidunt ea quibusdam alias! Nam itaque minus odio, ullam quod debitis voluptates natus in sit harum magni sapiente, impedit, ad sunt similique ratione?
+                Hic exercitationem suscipit eaque consequuntur, repudiandae voluptas qui quam odit perspiciatis sit eligendi ut maiores dolore velit quisquam quod ex in, omnis dicta reiciendis? Minus eos cum doloremque repellendus adipisci.
+                Delectus, ipsa officiis, facere atque, ex nemo et quod iusto quis magni sunt explicabo? Perferendis quas alias eaque, est voluptatibus vel, amet aspernatur assumenda consequuntur fugit at maiores, hic molestiae.
+                Temporibus sequi quis in vero, assumenda sapiente. Eius quibusdam delectus aperiam molestiae autem dolorum nostrum perspiciatis beatae distinctio, laudantium alias eveniet reiciendis quia consequuntur perferendis quasi aut ullam animi amet!
+            </VStack>
         </VStack>
     );
 });
